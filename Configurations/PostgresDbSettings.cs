ee0f00837e2ee41fd0734cf813f33b37de20d258
mosaab-rady <70973036+mosaab-rady@users.noreply.github.com>
2022-08-01 20:33:17 +0200

    remove nullable

--- Configurations/PostgresDbSettings.cs
+++ Configurations/PostgresDbSettings.cs
@@ -2,10 +2,10 @@ namespace mvcApp.Configurations;
 
 public class PostgresDbSettings
 {
-	public string? Host { get; set; }
-	public string? Username { get; set; }
-	public string? Database { get; set; }
-	public string? Password { get; set; }
+	public string Host { get; set; }
+	public string Username { get; set; }
+	public string Database { get; set; }
+	public string Password { get; set; }
 	public string ConnectionString
 	{
 		get
